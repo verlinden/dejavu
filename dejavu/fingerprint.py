6383028f8d3dc5d9ba7ac12a5e88e399d5d4704d
Steve Verlinden <steve@dandycoding.com>
2014-10-27 15:36:10 +0100

    default params

--- dejavu/fingerprint.py
+++ dejavu/fingerprint.py
@@ -23,12 +23,12 @@ DEFAULT_WINDOW_SIZE = 4096
 # Ratio by which each sequential window overlaps the last and the
 # next window. Higher overlap will allow a higher granularity of offset
 # matching, but potentially more fingerprints.
-DEFAULT_OVERLAP_RATIO = 0.4
+DEFAULT_OVERLAP_RATIO = 0.5
 
 ######################################################################
 # Degree to which a fingerprint can be paired with its neighbors --
 # higher will cause more fingerprints, but potentially better accuracy.
-DEFAULT_FAN_VALUE = 10
+DEFAULT_FAN_VALUE = 15
 
 ######################################################################
 # Minimum amplitude in spectrogram in order to be considered a peak.
@@ -40,7 +40,7 @@ DEFAULT_AMP_MIN = 10
 # Number of cells around an amplitude peak in the spectrogram in order
 # for Dejavu to consider it a spectral peak. Higher values mean less
 # fingerprints and faster matching, but can potentially affect accuracy.
-PEAK_NEIGHBORHOOD_SIZE = 30
+PEAK_NEIGHBORHOOD_SIZE = 20
 
 ######################################################################
 # Thresholds on how close or far fingerprints can be in time in order
@@ -53,7 +53,7 @@ MAX_HASH_TIME_DELTA = 200
 # If True, will sort peaks temporally for fingerprinting;
 # not sorting will cut down number of fingerprints, but potentially
 # affect performance.
-PEAK_SORT = False
+PEAK_SORT = True
 
 ######################################################################
 # Number of bits to throw away from the front of the SHA1 hash in the
